612b0c85679e14b0fe17837fb1afaaaaaccc8d80
rahepler2 <94093369+rahepler2@users.noreply.github.com>
2024-07-18 18:06:09 -0400

    Update app.py

--- app.py
+++ app.py
@@ -430,8 +430,6 @@ def user_login():
 
 if __name__ == '__main__':
     print("Starting Flask app...")
-    app.run(debug=True)
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run()
 
 
